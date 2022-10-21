3ddd341515ff96dbd97837a7a618e37cc651d899
Amalime Chika Samuel <samuelmereal@gmail.com>
2022-10-21 23:06:43 +0000

    this is first commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const username = prompt("what is your name?")
 console.log(`welcome ${username}`)
 console.log(`THIS IS A NUMBER GUESSING GAME`)
 
+//variables
 let Point = 0;
 let Level = 1;
 MaxStage = 100
@@ -16,16 +17,16 @@ for (let n=2; n<=100; n++){
   let comGuess = Math.floor(Math.random()*n)+1
   let userGuess = parseInt(prompt(`Guess a number between 1 and ${n}`));
   
-  console.log(userGuess);
+  console.log(`you guessed ${userGuess}`);
 
   if(comGuess == userGuess){
-    console.log(`correct`);
+    console.log(`your guess is correct`);
     console.log(`Point = ${ Point+= 1}`);
     console.log(`welcome to level ${Level +=1} `)
   } 
   else {
-    console.log(`wrong`);
+    console.log(`your guess is wrong`);
     console.log(`Point = ${ Point+= 0}`);
-    console.log(`welcome to level ${Level +=1} `)
+    console.log(`welcome to level ${Level +=1}`)
   }
 }
